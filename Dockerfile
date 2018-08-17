e73dd5c5a53852edbc150c2a8781166f68913aee
AciDCooL <arno.kattenpoel@gmail.com>
2018-08-18 00:52:13 +0200

    cloud pull

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
-FROM openjdk:8-jre-slim
+FROM openjdk:8-jre
 VOLUME /data
 EXPOSE 8080
 ENV ACTIVE_PROFILE=docker
 
 WORKDIR /app
 COPY docker/entrypoint.sh entrypoint.sh
-COPY docker/application.yml application.yml
-COPY build/libs/*.war streama.war
+COPY docker/application.yml /tmp/application.yml
+ADD docker/init.sh /app
+CMD /app/init.sh
 
 ENTRYPOINT ["sh", "entrypoint.sh"]
---
+++ docker/init.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+if [[ ! -e /data/application.yml ]]; then
+   cp -rf /tmp/application.yml /data/application.yml
+fi
+
+if [[ ! -e /app/application.yml ]]; then
+	ln -s /data/application.yml /app/application.yml
+  else
+	rm -rf /app/application.yml
+	ln -s /data/application.yml /app/application.yml
+fi
+
+if [[ ! -e /app/streama.war ]]; then
+   wget $(wget -qO- 'https://api.github.com/repos/streamaserver/streama/releases/latest' | grep browser_download_url | cut -d '"' -f 4)
+fi
